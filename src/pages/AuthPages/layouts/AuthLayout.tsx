6e4a2ca65ce86c9c101f8f597fb7112553a9f91f
Khaled Elgamal <khaled.elgamal.work@gmail.com>
2026-09-04 23:25:13 +0300

    feat(responsiveness) - update AuthLayout component for improved responsiveness

--- src/pages/AuthPages/layouts/AuthLayout.tsx
+++ src/pages/AuthPages/layouts/AuthLayout.tsx
@@ -1,3 +1,4 @@
+import { sectionPaddingX } from '@/constants/global.styles';
 import type { ReactNode } from 'react';
 
 interface AuthLayoutProps {
@@ -7,7 +8,9 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ title, children }: AuthLayoutProps) => {
   return (
-    <div className="w-[520px] rounded-lg bg-white p-8 shadow-md stroke-gray-50">
+    <div
+      className={`w-[520px] rounded-lg bg-white ${sectionPaddingX} shadow-md pb-2 stroke-gray-50`}
+    >
       <h2 className="mb-5 text-center text-[32px] font-semibold text-gray-900">{title}</h2>
       {children}
     </div>
